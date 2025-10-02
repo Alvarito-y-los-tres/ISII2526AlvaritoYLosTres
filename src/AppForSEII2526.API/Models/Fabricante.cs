906ffd1e56cf19aa46a5f677e38cbeb3d40f8510
ÁLVARO CANO ANDRÉS <Alvaro.Cano1@alu.uclm.es>
2025-10-02 18:38:14 +0200

    Modificación de clase Fabricante y creación de Herramienta.

--- src/AppForSEII2526.API/Models/Fabricante.cs
+++ src/AppForSEII2526.API/Models/Fabricante.cs
@@ -4,6 +4,8 @@
     {
         public int Id { get; set; }
         public string Nome { get; set; }
-      
+
+        public List<Herramienta> Herramientas { get; set; }
+
     }
 }
---
+++ src/AppForSEII2526.API/Models/Herramienta.cs
@@ -0,0 +1,13 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Herramienta
+    {
+        public int Id { get; set; }
+        public int Nombre { get; set; }
+        public string Material { get; set; }
+        public float Precio { get; set; }
+        public float TiempoReparacion { get; set; }
+        
+        public Fabricante Fabricante { get; set; }
+    }
+}
