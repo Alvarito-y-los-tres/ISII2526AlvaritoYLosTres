6d592bfc1621d64f9a8939bbadc3492e9d6e759f
DANIEL DE LA CRUZ TORRIJOS <Daniel.Cruz3@alu.uclm.es>
2025-10-02 18:26:39 +0200

    Cambio el dbContext y actualizacion clase fabricante

--- src/AppForSEII2526.API/Models/Fabricante.cs
+++ src/AppForSEII2526.API/Models/Fabricante.cs
@@ -3,7 +3,7 @@
     public class Fabricante
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
-      
+        public string Nombre { get; set; }
+        
     }
 }
