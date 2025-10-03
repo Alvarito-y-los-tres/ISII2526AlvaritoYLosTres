8637660ab45ff651919d0acd726c876cecea7d78
ANA LÓPEZ MARÍN <Ana.Lopez49@alu.uclm.es>
2025-10-03 20:07:37 +0200

    Añadir DbContext de clase Compra y CompraItem

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -7,4 +7,9 @@ namespace AppForSEII2526.API.Data;
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : 
     IdentityDbContext<ApplicationUser>(options) {
     public DbSet<Fabricante> Fabricantes { get; set; }
+
+    public DbSet<Herramienta> Herramientas { get; set; }
+    public DbSet<Compra> Compras { get; set; }
+    public DbSet<CompraItem> CompraItems { get; set; }
+
 }
