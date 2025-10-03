90f528857038ffea7bcb01af89c58ac8e80290ea
ANA LÓPEZ MARÍN <Ana.Lopez49@alu.uclm.es>
2025-10-03 20:09:19 +0200

    Creación clase CompraItem y Compra

---
+++ src/AppForSEII2526.API/Models/Compra.cs
@@ -0,0 +1,39 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Compra
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(30, ErrorMessage = "El nombre no puede tener más de 30 caracteres")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        public string ApellidoCLiente { get; set; }
+
+        [EmailAddress]
+        public string CorreoElectronico { get; set; }
+
+        [Required]
+        public string DireccionEnvio { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Date), Display(Name = "Release Date")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime FechaCompra { get; set; }
+
+       
+
+        [Required, StringLength(30, ErrorMessage = "El nombre no puede tener más de 30 caracteres")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        public string NombreCliente { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Display(Name = "Price For Renting")]
+        [Precision(5, 2)]
+        public decimal PrecioTotal { get; set; }
+
+        [Phone]
+        public int Telefono { get; set; }
+
+        public List<CompraItem> CompraItems { get; set; }
+    }
+}
---
+++ src/AppForSEII2526.API/Models/CompraItem.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Policy;
+
+namespace AppForSEII2526.API.Models
+{
+    public class CompraItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser un valor positivo")]
+        public int Cantidad { get; set; }
+
+        [Required]
+        [StringLength(200, ErrorMessage = "La descripción no puede tener más de 200 caracteres")]
+        public string Descripcion { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Display(Name = "Price For Renting")]
+        [Precision(5, 2)]
+        public decimal Precio { get; set; }
+
+
+        [Required]
+        public int IdHerramienta { get; set; }
+        public Herramienta Herramienta { get; set; } 
+
+
+        [Required]
+        public int IdCompra { get; set; }
+        public Compra Compra { get; set; }
+    }
+}
