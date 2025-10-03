b7f03ba298b6cf5d13a0d14275a7199ed6f0dd5a
ÁLVARO CANO ANDRÉS <Alvaro.Cano1@alu.uclm.es>
2025-10-03 19:46:26 +0200

    Creación de las clases OfertaItem y Oferta

---
+++ src/AppForSEII2526.API/Models/OfertaItem.cs
@@ -0,0 +1,29 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class OfertaItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int IdHerramienta { get; set; }
+        public Herramienta Herramienta { get; set; }
+
+        [Required]
+        public int IdOferta { get; set; }
+        public Oferta Oferta { get; set; }
+
+        [Required]
+        public float Porcentaje { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Display(Name = "Precio Final")]
+        [Precision(5, 2)]
+        public decimal PrecioFinal { get; set; }
+
+        
+        
+       
+
+    }
+}
