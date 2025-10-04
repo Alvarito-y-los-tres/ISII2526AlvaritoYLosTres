75f438d182c72848d71b4a2292f096ad21975e3d
6danicruz9 <Daniel.Cruz3@alu.uclm.es>
2025-10-04 17:02:04 +0200

    Crear clase AlquilarItem y alquiler (#25)

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -12,6 +12,11 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     public DbSet<Herramienta> Herramientas { get; set; }
     public DbSet<Compra> Compras { get; set; }
     public DbSet<CompraItem> CompraItems { get; set; }
+    public DbSet<Oferta> Ofertas { get; set; }
+    public DbSet<OfertaItem> OfertaItems { get; set; }
+    public DbSet<Alquiler> Alquileres { get; set; }
+    public DbSet<AlquilerItem> AlquilerItems { get; set; }
+
 
 }
 
