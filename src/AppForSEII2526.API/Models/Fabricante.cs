a94c799c7e3b30fa882a1450608539c2f33c40c1
AnaLopez0419 <127494779+AnaLopez0419@users.noreply.github.com>
2025-10-04 16:20:20 +0200

    15 crear clase compraitem y compra (#17)
    * Añadir DbContext de clase Compra y CompraItem
    
    * Anotaciones en los atributos de la clase Fabricante
    
    * Anotaciones en los atributos y añadir relación en la clase Herramienta
    
    * Creación clase CompraItem y Compra
    
    ---------
    
    Co-authored-by: 6danicruz9 <Daniel.Cruz3@alu.uclm.es>

--- src/AppForSEII2526.API/Models/Fabricante.cs
+++ src/AppForSEII2526.API/Models/Fabricante.cs
@@ -2,10 +2,14 @@
 {
     public class Fabricante
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required, StringLength(30, ErrorMessage = "El nombre no puede tener más de 30 caracteres")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
         public string Nombre { get; set; }
-                              
-        public List<Herramienta> Herramientas { get; set; }
+
+        public List<Herramienta> Herramientas { get; set; } 
 
     }
 }
--- src/AppForSEII2526.API/Models/Herramienta.cs
+++ src/AppForSEII2526.API/Models/Herramienta.cs
@@ -4,12 +4,25 @@
     {
         [Key]
         public int Id { get; set; }
-        public int Nombre { get; set; }
+
+        [Required, StringLength(30, ErrorMessage = "El nombre no puede tener más de 30 caracteres")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        public string Nombre { get; set; }
+
+        [Required, StringLength(30, ErrorMessage = "El material no puede tener más de 30 caracteres")]
         public string Material { get; set; }
-        public float Precio { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Display(Name = "Price For Renting")]
+        [Precision(18, 2)]
+        public decimal Precio { get; set; }
+
         public float TiempoReparacion { get; set; }
-        
+
         public Fabricante Fabricante { get; set; }
+        
+
+        public List<CompraItem> CompraItems { get; set; } 
 
         public List<OfertaItem> OfertaItems { get; set; }
     }
