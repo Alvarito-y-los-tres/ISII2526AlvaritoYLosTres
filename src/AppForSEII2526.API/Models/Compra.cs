5abe8ddb5f9593931cde0fc6daaffa902ccc5770
ANA LÓPEZ MARÍN <Ana.Lopez49@alu.uclm.es>
2025-10-09 09:32:18 +0200

    Añadir relación con ApplicationUser y eliminar atributos que son de usuario

--- src/AppForSEII2526.API/Models/Compra.cs
+++ src/AppForSEII2526.API/Models/Compra.cs
@@ -6,13 +6,6 @@
         [Key]
         public int Id { get; set; }
 
-        [Required, StringLength(30, ErrorMessage = "El nombre no puede tener más de 30 caracteres")]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
-        public string ApellidoCLiente { get; set; }
-
-        [EmailAddress]
-        public string CorreoElectronico { get; set; }
-
         [Required]
         public string DireccionEnvio { get; set; }
 
@@ -20,23 +13,18 @@
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime FechaCompra { get; set; }
 
-       
-
-        [Required, StringLength(30, ErrorMessage = "El nombre no puede tener más de 30 caracteres")]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
-        public string NombreCliente { get; set; }
 
         [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
         [Display(Name = "Price For Renting")]
         [Precision(5, 2)]
         public decimal PrecioTotal { get; set; }
 
-        [Phone]
-        public int Telefono { get; set; }
 
         [Required]
         public TiposMetodosPago MetodoPago { get; set; }
 
         public List<CompraItem> CompraItems { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
     }
 }
