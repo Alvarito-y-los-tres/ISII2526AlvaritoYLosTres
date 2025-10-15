1e9c9d5ac6abdc9d210a37466bf8221ea8581e96
6danicruz9 <Daniel.Cruz3@alu.uclm.es>
2025-10-15 12:22:00 +0200

    Crear carpeta Controllers (#65)

---
+++ src/AppForSEII2526.API/Controllers/HerramientaController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HerramientaController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+        private readonly ILogger<HerramientaController> _logger;
+
+        public HerramientaController(ApplicationDBContext context, ILogger<HerramientaController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")])]
+        [ProduceResponseType(typeof(IList<Herramienta>), StatusCodes.Status200OK)]))]
+        public asynv Task<IActionResult> GetAllHerramientas()
+        {
+            IList<Herramienta> herramientas = await _context.Herramientas.ToListAsync();
+              return Ok(herramientas);
+        }
+    }
+}
