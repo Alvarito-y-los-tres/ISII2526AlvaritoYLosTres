2e400957ae9249b921cb4f78a346282a430212c9
ANA LÓPEZ MARÍN <Ana.Lopez49@alu.uclm.es>
2025-10-20 18:13:07 +0200

    Creación del get con sus filtros para el CU Comprar Herramientas

--- src/AppForSEII2526.API/Controllers/HerramientaController.cs
+++ src/AppForSEII2526.API/Controllers/HerramientaController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AppForSEII2526.API.DTOs;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace AppForSEII2526.API.Controllers
 {
@@ -7,22 +9,39 @@ namespace AppForSEII2526.API.Controllers
     [ApiController]
     public class HerramientaController : ControllerBase
     {
-        private readonly ApplicationDBContext _context;
+        private readonly ApplicationDbContext _context;
         private readonly ILogger<HerramientaController> _logger;
 
-        public HerramientaController(ApplicationDBContext context, ILogger<HerramientaController> logger)
+        public HerramientaController(ApplicationDbContext context, ILogger<HerramientaController> logger)
         {
             _context = context;
             _logger = logger;
         }
 
         [HttpGet]
-        [Route("[action]")])]
-        [ProduceResponseType(typeof(IList<Herramienta>), StatusCodes.Status200OK)]))]
-        public asynv Task<IActionResult> GetAllHerramientas()
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<Herramienta>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetAllHerramientas()
         {
             IList<Herramienta> herramientas = await _context.Herramientas.ToListAsync();
               return Ok(herramientas);
         }
+
+
+        [HttpGet]
+        [Route("Para-Comprar")]
+        [ProducesResponseType(typeof(IList<HerramientasParaComprarDTO>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetHerramientaParaComprar(string? material, decimal? precio)
+        {
+            IList<HerramientasParaComprarDTO> selectHerramienta = await _context.Herramientas
+                .Include(h => h.Fabricante)
+                .Include(h => h.CompraItems).ThenInclude(pi => pi.Compra)
+                .Where(h => (material == null || h.Material == material) &&
+                            (precio == null || h.Precio <= precio))
+                .Select(h => new HerramientasParaComprarDTO(h.Nombre, h.Material, h.Precio, h.Fabricante.Nombre))
+                .ToListAsync();
+            return Ok(selectHerramienta);
+        }
+
     }
 }
