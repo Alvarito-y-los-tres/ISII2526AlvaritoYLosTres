3b9eb9197fa46779684148af49264e1daedb4e01
AnaLopez0419 <127494779+AnaLopez0419@users.noreply.github.com>
2025-10-25 18:26:39 +0200

    74 implementar método get del detalle de caso de uso comprar herramientas (#86)
    * Creación de los DTOs Detalle e Item para CU Comprar Herramienta
    
    * Creación controller para cu Comprar Herramienta

---
+++ src/AppForSEII2526.API/DTOs/CompraDetalleDTO.cs
@@ -0,0 +1,47 @@
+﻿
+
+namespace AppForSEII2526.API.DTOs
+{
+    public class CompraDetalleDTO
+    {
+        public int Id { get; set; }
+        public string NombreCLiente { get; set; }
+        public string ApellidoCliente { get; set; }
+
+        public string DireccionEnvio { get; set; }
+        public decimal PrecioTotal { get; set; }
+        public DateTime FechaCompra { get; set; }
+
+
+        public IList<CompraItemDTO> CompraItemDTO { get; set; }
+
+        public CompraDetalleDTO(int id, string nombreCLiente, string apellidoCliente, string direccionEnvio, decimal precioTotal, DateTime fechaCompra, IList<CompraItemDTO> compraItemDTO)
+        {
+            Id = id;
+            NombreCLiente = nombreCLiente;
+            ApellidoCliente = apellidoCliente;
+            DireccionEnvio = direccionEnvio;
+            PrecioTotal = precioTotal;
+            FechaCompra = fechaCompra;
+            CompraItemDTO = compraItemDTO;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is CompraDetalleDTO dTO &&
+                   Id == dTO.Id &&
+                   NombreCLiente == dTO.NombreCLiente &&
+                   ApellidoCliente == dTO.ApellidoCliente &&
+                   DireccionEnvio == dTO.DireccionEnvio &&
+                   PrecioTotal == dTO.PrecioTotal &&
+                   FechaCompra == dTO.FechaCompra &&
+                   EqualityComparer<IList<CompraItemDTO>>.Default.Equals(CompraItemDTO, dTO.CompraItemDTO);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, NombreCLiente, ApellidoCliente, DireccionEnvio, PrecioTotal, FechaCompra, CompraItemDTO);
+        }
+    }
+}
+    
