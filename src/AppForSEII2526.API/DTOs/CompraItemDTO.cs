7a5cc072cc0f1269735b6b7fa5e9dc022fb8f0b1
ANA LÓPEZ MARÍN <Ana.Lopez49@alu.uclm.es>
2025-10-26 17:08:19 +0100

    quitar atributo 'id' de los dtos

--- src/AppForSEII2526.API/DTOs/CompraItemDTO.cs
+++ src/AppForSEII2526.API/DTOs/CompraItemDTO.cs
@@ -4,16 +4,16 @@ namespace AppForSEII2526.API.DTOs
 {
     public class CompraItemDTO
     {
-        public int HerramientaId { get; set; }
+        
         public string NombreHerramienta { get; set; }
         public string MaterialHerrramienta { get; set; }
         public decimal PrecioHerramienta { get; set; }
         public string DescripcionHerramienta { get; set; }
         public int Cantidad { get; set; }
 
-        public CompraItemDTO(int herramientaId, string nombreHerramienta, string materialHerrramienta, decimal precioHerramienta, string descripcionHerramienta, int cantidad)
+        public CompraItemDTO(string nombreHerramienta, string materialHerrramienta, decimal precioHerramienta, string descripcionHerramienta, int cantidad)
         {
-            HerramientaId = herramientaId;
+            
             NombreHerramienta = nombreHerramienta;
             MaterialHerrramienta = materialHerrramienta;
             PrecioHerramienta = precioHerramienta;
@@ -24,7 +24,6 @@ namespace AppForSEII2526.API.DTOs
         public override bool Equals(object? obj)
         {
             return obj is CompraItemDTO dTO &&
-                   HerramientaId == dTO.HerramientaId &&
                    NombreHerramienta == dTO.NombreHerramienta &&
                    MaterialHerrramienta == dTO.MaterialHerrramienta &&
                    PrecioHerramienta == dTO.PrecioHerramienta &&
@@ -34,7 +33,7 @@ namespace AppForSEII2526.API.DTOs
 
         public override int GetHashCode()
         {
-            return HashCode.Combine(HerramientaId, NombreHerramienta, MaterialHerrramienta, PrecioHerramienta, DescripcionHerramienta, Cantidad);
+            return HashCode.Combine(NombreHerramienta, MaterialHerrramienta, PrecioHerramienta, DescripcionHerramienta, Cantidad);
         }
     }
 }
