2d7ee1a2df8fb6aa1cd20c80acb7a317aef4c25c
alvarooo19 <alvaro.cano1@alu.uclm.es>
2025-10-26 13:05:46 +0100

    73 implementar método get del detalle de caso de uso oferta herramienta (#87)
    * Creación de los DTOS para el caso de uso Oferta Herramientas
    
    * Creación del controlador de ofertas
    
    * Creación de los documentos .sql para la base de datos

---
+++ src/AppForSEII2526.API/DTOs/OfertaDetalleDTO.cs
@@ -0,0 +1,41 @@
+﻿
+
+
+namespace AppForSEII2526.API.DTOs
+{
+    public class OfertaDetalleDTO
+    {
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public DateTime FechaOferta { get; set; }
+        public TiposDirigidaOferta ParaSocios { get; set; }
+        public TiposMetodosPago MetodoPago { get; set; }
+        public IList<OfertaItemDTO> OfertaItems { get; set; }
+
+        public OfertaDetalleDTO(DateTime fechaInicio, DateTime fechaFin, DateTime fechaOferta, TiposDirigidaOferta paraSocios, TiposMetodosPago metodoPago, IList<OfertaItemDTO> ofertaItems)
+        {
+            FechaInicio = fechaInicio;
+            FechaFin = fechaFin;
+            FechaOferta = fechaOferta;
+            ParaSocios = paraSocios;
+            MetodoPago = metodoPago;
+            OfertaItems = ofertaItems;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is OfertaDetalleDTO dTO &&
+                   FechaInicio == dTO.FechaInicio &&
+                   FechaFin == dTO.FechaFin &&
+                   FechaOferta == dTO.FechaOferta &&
+                   ParaSocios == dTO.ParaSocios &&
+                   MetodoPago == dTO.MetodoPago &&
+                   EqualityComparer<IList<OfertaItemDTO>>.Default.Equals(OfertaItems, dTO.OfertaItems);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(FechaInicio, FechaFin, FechaOferta, ParaSocios, MetodoPago, OfertaItems);
+        }
+    }
+}
---
+++ src/AppForSEII2526.API/Data/SQL/dbo.Ofertas.data.sql
@@ -0,0 +1,7 @@
+﻿SET IDENTITY_INSERT [dbo].[Ofertas] ON;
+INSERT INTO [dbo].[Ofertas] ([Id], [FechaInicio], [FechaFin], [FechaOferta], [MetodoPago], [ParaSocio], [ApplicationUserId]) VALUES (1, N'2025-11-28 10:40:00', N'2025-11-30 23:59:59', N'2025-11-15 09:00:00', 1, 1, N'1')
+INSERT INTO [dbo].[Ofertas] ([Id], [FechaInicio], [FechaFin], [FechaOferta], [MetodoPago], [ParaSocio], [ApplicationUserId]) VALUES (2, N'2025-12-01 08:00:00', N'2025-12-24 23:59:59', N'2025-11-20 10:00:00', 2, 2, N'2')
+INSERT INTO [dbo].[Ofertas] ([Id], [FechaInicio], [FechaFin], [FechaOferta], [MetodoPago], [ParaSocio], [ApplicationUserId]) VALUES (3, N'2026-01-07 07:00:00', N'2026-01-31 23:59:59', N'2026-01-06 17:00:00', 1, 1, N'3')
+INSERT INTO [dbo].[Ofertas] ([Id], [FechaInicio], [FechaFin], [FechaOferta], [MetodoPago], [ParaSocio], [ApplicationUserId]) VALUES (4, N'2025-10-25 08:00:00', N'2025-10-27 08:00:00', N'2025-10-25 07:30:00', 3, 2, N'4')
+INSERT INTO [dbo].[Ofertas] ([Id], [FechaInicio], [FechaFin], [FechaOferta], [MetodoPago], [ParaSocio], [ApplicationUserId]) VALUES (5, N'2025-07-01 18:00:00', N'2025-07-31 23:59:59', N'2025-06-15 12:00:00', 2, 2, N'5')
+SET IDENTITY_INSERT [dbo].[Ofertas] OFF;
