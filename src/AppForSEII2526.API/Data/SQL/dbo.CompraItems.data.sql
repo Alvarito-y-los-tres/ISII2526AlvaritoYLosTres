324ff2696df67d4aa3ffbb00562f3fe9e5719b0a
AnaLopez0419 <127494779+AnaLopez0419@users.noreply.github.com>
2025-10-26 18:59:04 +0100

    creación de archivos sql para CompraItems y Compras (#95)

---
+++ src/AppForSEII2526.API/Data/SQL/dbo.CompraItems.data.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO [dbo].[CompraItems] ([HerramientaId], [CompraId], [Cantidad], [Descripcion], [Precio]) VALUES (1, 3, 1, N'grande', CAST(35.00 AS Decimal(5, 2)))
+INSERT INTO [dbo].[CompraItems] ([HerramientaId], [CompraId], [Cantidad], [Descripcion], [Precio]) VALUES (2, 4, 1, N'pequeño', CAST(3.00 AS Decimal(5, 2)))
+INSERT INTO [dbo].[CompraItems] ([HerramientaId], [CompraId], [Cantidad], [Descripcion], [Precio]) VALUES (3, 5, 1, N'grande', CAST(5.00 AS Decimal(5, 2)))
