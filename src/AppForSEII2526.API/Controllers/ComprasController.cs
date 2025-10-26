a38f9dd361492c8b498e9fc9b661455795e680ce
ANA LÓPEZ MARÍN <Ana.Lopez49@alu.uclm.es>
2025-10-26 17:07:55 +0100

    corrección del controlador del get de detalle del cu Comprar Herramientas

--- src/AppForSEII2526.API/Controllers/ComprasController.cs
+++ src/AppForSEII2526.API/Controllers/ComprasController.cs
@@ -1,97 +1,70 @@
 ﻿using AppForSEII2526.API.DTOs;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
 
 namespace AppForSEII2526.API.Controllers
 {
-
-        [Route("api/[controller]")]
-        [ApiController]
-        public class ComprasController : ControllerBase
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComprasController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<HerramientaController> _logger;
+        public ComprasController(ApplicationDbContext context, ILogger<HerramientaController> logger)
         {
-            private readonly ApplicationDbContext _context;
-            private readonly ILogger<ComprasController> _logger;
+            _context = context;
+            _logger = logger;
+        }
 
-            public ComprasController(ApplicationDbContext context, ILogger<ComprasController> logger)
+
+        [HttpGet]
+        [Route("Compra-Detalle")]
+        [ProducesResponseType(typeof(IList<CompraDetalleDTO>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetCompraDetalle()
+        {
+            if (_context.Compras == null)
             {
-                _context = context;
-                _logger = logger;
+                _logger.LogError("Error: La tabla no existe.");
+                return NotFound();
             }
 
-            [HttpGet]
-            [Route("[action]")]
-            [ProducesResponseType(typeof(IList<CompraDetalleDTO>), (int)HttpStatusCode.OK)]
-            [ProducesResponseType((int)HttpStatusCode.NotFound)]
-            public async Task<IActionResult> GetDetalleHerramientasParaComprar(int id)
-            {
-                if (_context.Compras == null)
-                {
-                    _logger.LogError("Error: No se encontraron compras en la base de datos");
-                    return NotFound("No hay compras registradas");
-                }
+            IList<CompraDetalleDTO> compraDetalles = await _context.Compras
+                .Include(o => o.MetodoPago)
+                .Include(o => o.ApplicationUser)
+                .Include(o => o.CompraItems)
+                    .ThenInclude(oi => oi.Herramienta)
+                    .ThenInclude(h => h.Fabricante)
+                
 
-            var compras = await _context.Compras
-             .Where(c => c.Id == id)
-                .Include(c => c.CompraItems) 
-                 .ThenInclude(ci => ci.Herramienta)
-                    .ThenInclude(h => h.Fabricante) 
-
-             .Select(c => new CompraDetalleDTO(
-                 c.Id, 
-                 c.ApplicationUser.NombreCliente,
-                 c.ApplicationUser.ApellidoCliente,
-                 c.DireccionEnvio,
-                 c.PrecioTotal,
-                 c.FechaCompra,
-                 c.CompraItems
-
-                    .Select(ci => new CompraItemDTO(
-                        ci.Herramienta.Id,
-                        ci.Herramienta.Nombre,
-                        ci.Herramienta.Material,
-                        ci.Precio,
-                        ci.Descripcion,
-                        ci.Cantidad)).ToList<CompraItemDTO>()))
-         .FirstOrDefaultAsync();
-
-            /*
-            var comprasDetalle = await _context.Compras
-                .Include(c => c.MetodoPago)
-                .Include(c => c.ApplicationUser)
-                .Include(c => c.CompraItems)
-                    .ThenInclude(ci => ci.Herramienta)
-                        .ThenInclude(h => h.Fabricante)
+                .Select(o => new CompraDetalleDTO(
+                    o.ApplicationUser.NombreCliente,
+                    o.ApplicationUser.ApellidoCliente,
+                    o.DireccionEnvio,
+                    o.PrecioTotal,
+                    o.FechaCompra,
+                    o.CompraItems.Select(oi => new CompraItemDTO(
+                        oi.Herramienta.Nombre,
+                        oi.Herramienta.Material,
+                        oi.Herramienta.Precio,
+                        oi.Descripcion,
+                        oi.Cantidad
+                        )).ToList()
+                ))
                 .ToListAsync();
 
 
-            var compraDetalleDTOs = comprasDetalle.Select(c => new CompraDetalleDTO(
-                c.ApplicationUser.NombreCliente,
-                c.ApplicationUser.ApellidoCliente,
-                c.DireccionEnvio,
-                c.PrecioTotal,
-                c.FechaCompra,
-                c.CompraItems.Select(ci => new CompraItemDTO(
-                    ci.Herramienta.Nombre,
-                    ci.Herramienta.Material,
-                    ci.Herramienta.Precio,
-                    ci.Descripcion,
-                    ci.Cantidad
-                )).ToList()
-            )).ToList();
-
-            */
-
-            if (compras == null)
-                {
-                    _logger.LogError("Error: No se pudieron mapear las compras a DTOs");
-                    return NotFound("No se encontraron detalles de compras");
-                }
-
-                return Ok(compras);
-
-
+            if (compraDetalles == null)
+            {
+                _logger.LogError("No se encontraron compras en la base de datos.");
+                return NotFound();
             }
+
+            return Ok(compraDetalles);
+
         }
     }
 
-    
+
+
+}
