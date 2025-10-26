641e7c3e0cebe1419062027fbfe5cb869521a287
ÁLVARO CANO ANDRÉS <Alvaro.Cano1@alu.uclm.es>
2025-10-26 12:58:33 +0100

    Creación de los DTOS para el caso de uso Oferta Herramientas

---
+++ src/AppForSEII2526.API/DTOs/OfertaItemDTO.cs
@@ -0,0 +1,37 @@
+﻿
+
+namespace AppForSEII2526.API.DTOs
+{
+    public class OfertaItemDTO
+    {
+        public string NombreHerramienta { get; set; }
+        public string MaterialHerramienta { get; set; }
+        public string FabricanteHerramienta { get; set; }
+        public decimal Precio { get; set; }
+        public decimal PrecioFinal { get; set; }
+
+        public OfertaItemDTO(string nombreHerramienta, string materialHerramienta, string fabricanteHerramienta, decimal precio, decimal precioFinal)
+        {
+            NombreHerramienta = nombreHerramienta;
+            MaterialHerramienta = materialHerramienta;
+            FabricanteHerramienta = fabricanteHerramienta;
+            Precio = precio;
+            PrecioFinal = precioFinal;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is OfertaItemDTO dTO &&
+                   NombreHerramienta == dTO.NombreHerramienta &&
+                   MaterialHerramienta == dTO.MaterialHerramienta &&
+                   FabricanteHerramienta == dTO.FabricanteHerramienta &&
+                   Precio == dTO.Precio &&
+                   PrecioFinal == dTO.PrecioFinal;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(NombreHerramienta, MaterialHerramienta, FabricanteHerramienta, Precio, PrecioFinal);
+        }
+    }
+}
