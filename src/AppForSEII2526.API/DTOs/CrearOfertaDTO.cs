be8409aa6d3cdf0d109a7ac4fc8141de27a79bd1
ÁLVARO CANO ANDRÉS <Alvaro.Cano1@alu.uclm.es>
2025-10-30 16:28:55 +0100

    Implementación del post del método Oferta Herramienta, creación del último DTO y modificación de OfertaItemDTO

---
+++ src/AppForSEII2526.API/DTOs/CrearOfertaDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace AppForSEII2526.API.DTOs
+{
+    public class CrearOfertaDTO
+    {
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public DateTime FechaOferta { get; set; }
+        public List<OfertaItemDTO> Items { get; set; }
+        public int MetodoPago { get; set; }
+        public int ParaSocio { get; set; }
+
+        public CrearOfertaDTO()
+        {
+            Items = new List<OfertaItemDTO>();
+        }
+    }
+}
---
+++ test/AppForSEII2526.UT/HerramientasController_test/GetHerramientaParaOferta_test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UT.HerramientasController_test
+{
+    public class GetHerramientaParaOferta_test: AppForSEII25264SqliteUT
+    {
+        public GetHerramientaParaOferta_test()
+        {
+            var fabricante = new List<Fabricante>
+            {
+                new Fabricante ("Ferreteía López"),
+                new Fabricante ("Ferreteía García"),
+                new Fabricante ("Ferreteía Ruiz")
+            };
+
+            var herramientas = new List<Herramienta>
+            {
+                new Herramienta ("Martillo", "Madera", 35, 3, fabricante[0]),
+                new Herramienta ("Alicates", "Acero", 12, 1, fabricante[1]),
+                new Herramienta ("Tijeras", "Plastico", 3, 1, fabricante[2])
+            };
+
+            ApplicationUser user = new ApplicationUser("Álvaro", "Cano Andrés", "600123456", "alvaro@gmail.com");
+
+            
+        }
+    }
+}
