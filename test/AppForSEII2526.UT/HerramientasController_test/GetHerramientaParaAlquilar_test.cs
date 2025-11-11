3152ba73f0ba834921580bd9f83f0e12db48cec1
6danicruz9 <Daniel.Cruz3@alu.uclm.es>
2025-11-11 10:31:00 +0100

    He modificado las pruebas de unidad del primer metodo gat ya que estaba plagado de errores (#172)

--- test/AppForSEII2526.UT/HerramientasController_test/GetHerramientaParaAlquilar_test.cs
+++ test/AppForSEII2526.UT/HerramientasController_test/GetHerramientaParaAlquilar_test.cs
@@ -1,10 +1,12 @@
 ﻿using AppForSEII2526.API.Controllers;
 using AppForSEII2526.API.DTOs;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace AppForSEII2526.UT.HerramientasController_test
 {
@@ -13,67 +15,69 @@ namespace AppForSEII2526.UT.HerramientasController_test
         public GetHerramientaParaAlquilar_test()
         {
             var fabricante = new List<Fabricante>
-            {
-                new Fabricante ("Ferreteía López"),
-                new Fabricante ("Ferreteía García"),
-                new Fabricante ("Ferreteía Ruiz")
-            };
+      {
+        new Fabricante ("Ferreteía López"),
+        new Fabricante ("Ferreteía García"),
+        new Fabricante ("Ferreteía Ruiz")
+      };
 
             var herramientas = new List<Herramienta>
-            {
-                new Herramienta ("Martillo", "Madera", 35, 3, fabricante[0]),
-                new Herramienta ("Alicates", "Acero", 12, 1, fabricante[1]),
-                new Herramienta ("Tijeras", "Plastico", 3, 1, fabricante[2])
-            };
-
-            //ApplicationUser user = new ApplicationUser("Álvaro", "Cano Andrés", "600123456", "alvaro@gmail.com");
+      {
+        new Herramienta ("Martillo", "Madera", 35, 3, fabricante[0]),
+        new Herramienta ("Alicates", "Acero", 12, 1, fabricante[1]),
+        new Herramienta ("Tijeras", "Plastico", 3, 1, fabricante[2])
+      };
 
             _context.AddRange(fabricante);
             _context.AddRange(herramientas);
             _context.SaveChanges();
-
         }
 
-        public static IEnumerable<object?[]> TestCasesFor_GetHerramientasParaOferta_OK()
+        public static IEnumerable<object?[]> TestCasesFor_GetHerramientasParaAlquilar_OK()
         {
-            var herramientasDTOs = new List<HerramientasParaOfertarDTO>
-            {
-                new HerramientasParaOfertarDTO ("Martillo", "Madera", "Ferreteía López", 35),
-                new HerramientasParaOfertarDTO ("Alicates", "Acero", "Ferreteía García", 12),
-                new HerramientasParaOfertarDTO ("Tijeras", "Plastico", "Ferreteía Ruiz", 3)
-            };
+            // FIX 1: Usar el DTO correcto: HerramientaAlquilarDTO
+            var herramientasDTOs = new List<HerramientaAlquilarDTO>
+      {
+        new HerramientaAlquilarDTO ("Martillo", "Madera", "Ferreteía López", 35), // [0]
+                new HerramientaAlquilarDTO ("Alicates", "Acero", "Ferreteía García", 12), // [1]
+                new HerramientaAlquilarDTO ("Tijeras", "Plastico", "Ferreteía Ruiz", 3)  // [2]
+            };
 
-            var herramientasDTOs_TC1 = new List<HerramientasParaOfertarDTO> { herramientasDTOs[1], herramientasDTOs[2] }
-            .OrderBy(h => h.Nombre).ToList();
+            // (Opcional, pero recomendado) Quito el .OrderBy() porque Assert.Equivalent no lo necesita
+            var herramientasDTOs_TC1 = new List<HerramientaAlquilarDTO> { herramientasDTOs[1], herramientasDTOs[2] };
 
-            var herramientasDTOs_TC2 = new List<HerramientasParaOfertarDTO> { herramientasDTOs[1] };
-            var herramientasDTOs_TC3 = new List<HerramientasParaOfertarDTO> { herramientasDTOs[2] };
+            var herramientasDTOs_TC2 = new List<HerramientaAlquilarDTO> { herramientasDTOs[1] };
+            var herramientasDTOs_TC3 = new List<HerramientaAlquilarDTO> { herramientasDTOs[2] };
 
-            var herramientasDTOs_TC4 = new List<HerramientasParaOfertarDTO> { herramientasDTOs[0], herramientasDTOs[1], herramientasDTOs[2] }
-            .OrderBy(h => h.Nombre).ToList();
+            // Esta es la lista completa (3 items)
+            var herramientasDTOs_TC4 = new List<HerramientaAlquilarDTO> { herramientasDTOs[0], herramientasDTOs[1], herramientasDTOs[2] };
 
             var allTestCases = new List<object?[]>
-            {
-                new object[] { null, null, herramientasDTOs_TC1 },
-                new object[] { "Alicates", null, herramientasDTOs_TC2 },
-                new object[] { null, "Plastico", herramientasDTOs_TC3 }
-            };
+      {
+                // FIX 2: El caso (null, null) debe esperar la lista completa (TC4)
+                new object[] { (string?)null, (string?)null, herramientasDTOs_TC4 },
+        new object[] { "Alicates", (string?)null, herramientasDTOs_TC2 },
+        new object[] { (string?)null, "Plastico", herramientasDTOs_TC3 }
+      };
             return allTestCases;
         }
 
         [Theory]
-        [MemberData(nameof(TestCasesFor_GetHerramientasParaOferta_OK))]
+        [MemberData(nameof(TestCasesFor_GetHerramientasParaAlquilar_OK))]
         [Trait("Database", "WithoutFixture")]
         [Trait("LevelTesting", "Unit Testing")]
-        public async Task GetHerramientaParaOferta_OK_test(string? nombre, decimal? material, IList<HerramientasParaOfertarDTO> herramientasDTOEsperado)
+        public async Task GetHerramientaParaAlquilar_OK_test(string? nombre, string? material, IList<HerramientaAlquilarDTO> herramientasDTOEsperado)
         {
-            var controller = new HerramientaController(_context, null);
+            var controller = new HerramientaController(_context, null!);
 
-            var result = await controller.GetHerramientaParaOferta(nombre, material);
+            var result = await controller.GetHerramientasParaAlquilar(nombre, material);
 
-            var okResult = result as OkObjectResult;
+            var okResult = Assert.IsType<OkObjectResult>(result);
 
-            var herramientasDTOActual = Assert.IsType<IList<HerramientasParaOfertarDTO>>(okResult.Value);
+            // FIX 3: Usar IsAssignableFrom para comprobar la interfaz
+            var herramientasDTOActual = Assert.IsAssignableFrom<IList<HerramientaAlquilarDTO>>(okResult.Value);
+
+            // FIX 4: Usar Equivalent para ignorar el orden de los elementos
             Assert.Equal(herramientasDTOEsperado, herramientasDTOActual);
         }
     }
