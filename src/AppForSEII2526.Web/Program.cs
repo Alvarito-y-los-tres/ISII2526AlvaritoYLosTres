168c03bf8ef172ecbd009fc15c8043b74c1bf80c
AnaLopez0419 <127494779+AnaLopez0419@users.noreply.github.com>
2025-11-12 12:52:43 +0100

    cambios requeridos para conectar la web con la api (#177)

--- src/AppForSEII2526.Web/Program.cs
+++ src/AppForSEII2526.Web/Program.cs
@@ -4,6 +4,8 @@ using Microsoft.EntityFrameworkCore;
 using AppForSEII2526.Web.Components;
 using AppForSEII2526.Web.Components.Account;
 using AppForSEII2526.Web.Data;
+using AppForSEII2526.Web.API;
+using ApplicationUser = AppForSEII2526.Web.Data.ApplicationUser;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -34,6 +36,12 @@ builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.Requ
     .AddDefaultTokenProviders();
 
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
+//this variable obtains  the url where the api has been deployed
+string? URI2API = builder.Configuration.GetValue(typeof(string),
+    "AppForHerramientas_API") as string;
+
+builder.Services.AddScoped<AppForSEII2526APIClient>(sp => new AppForSEII2526APIClient(URI2API, new HttpClient()));
 
 var app = builder.Build();
 
