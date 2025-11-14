cb5d7c1f0906c91cf501c23109c286625764f7f2
alvarooo19 <alvaro.cano1@alu.uclm.es>
2025-11-14 09:30:51 +0100

    178 creación de las pruebas de unidad para el post (#181)
    * modificacion necesarias para la creación de la prueba de unidad del metodo post
    
    * corrección de errores en el post
    
    * creación de la prueba de unidad del metodo post

--- src/AppForSEII2526.API/DTOs/CrearOfertaDTO.cs
+++ src/AppForSEII2526.API/DTOs/CrearOfertaDTO.cs
@@ -8,11 +8,39 @@
         public List<OfertaItemDTO> Items { get; set; }
         public int MetodoPago { get; set; }
         public int ParaSocio { get; set; }
-        public string nombreUsuario { get; set; }
+        public string NombreUsuario { get; set; }
 
         public CrearOfertaDTO()
         {
             Items = new List<OfertaItemDTO>();
+        }
+
+        public CrearOfertaDTO(DateTime fechaInicio, DateTime fechaFin, DateTime fechaOferta, List<OfertaItemDTO> items, int metodoPago, int paraSocio, string nombreUsuario)
+        {
+            FechaInicio = fechaInicio;
+            FechaFin = fechaFin;
+            FechaOferta = fechaOferta;
+            Items = items;
+            MetodoPago = metodoPago;
+            ParaSocio = paraSocio;
+            NombreUsuario = nombreUsuario;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is CrearOfertaDTO dTO &&
+                   FechaInicio == dTO.FechaInicio &&
+                   FechaFin == dTO.FechaFin &&
+                   FechaOferta == dTO.FechaOferta &&
+                   Items.SequenceEqual(dTO.Items) &&
+                   MetodoPago == dTO.MetodoPago &&
+                   ParaSocio == dTO.ParaSocio &&
+                   NombreUsuario == dTO.NombreUsuario;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(FechaInicio, FechaFin, FechaOferta, Items, MetodoPago, ParaSocio, NombreUsuario);
         }
     }
 }
