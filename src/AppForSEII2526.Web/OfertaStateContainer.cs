ef6091b4221db2d28ea9a4c4972f1b486b1f74c6
ÁLVARO CANO ANDRÉS <Alvaro.Cano1@alu.uclm.es>
2025-12-02 19:39:46 +0100

    Creación de mi apartado Oferta en la web, implementación del metodo select con los filtros, creación del StageContainer para la oferta y lo añadi al program.cs

---
+++ src/AppForSEII2526.Web/OfertaStateContainer.cs
@@ -0,0 +1,57 @@
+﻿using AppForSEII2526.Web.API;
+
+namespace AppForSEII2526.Web
+{
+    public class OfertaStateContainer
+    {
+        public CrearOfertaDTO Oferta { get; private set; } = new CrearOfertaDTO()
+        {
+            Items = new List<OfertaItemDTO>()
+        };
+
+        public decimal PrecioFinal 
+            {
+            get
+            {
+                return (decimal)Oferta.Items.Sum(i => i.Precio);
+            }
+        }
+
+        public event Action? OnChange;
+
+        public void NotifyStateChanged() => OnChange?.Invoke();
+
+        public void AñadirHerramientaToOferta(HerramientasParaOfertarDTO herramienta)
+        {
+            if (!Oferta.Items.Any(i => i.NombreHerramienta == herramienta.Nombre))
+            {
+                Oferta.Items.Add(new OfertaItemDTO()
+                {
+                    NombreHerramienta = herramienta.Nombre,
+                    MaterialHerramienta = herramienta.Material,
+                    FabricanteHerramienta = herramienta.Fabricante,
+                    Precio = herramienta.Precio
+                });
+                NotifyStateChanged();
+            }
+        }
+
+        public void EliminarHerramientaDeOferta(OfertaItemDTO item)
+        {
+            Oferta.Items.Remove(item);
+        }
+
+        public void LimpiarCarritoOferta()
+        {
+            Oferta.Items.Clear();
+        }
+
+        public void OfertaPreparada()
+        {
+            Oferta = new CrearOfertaDTO()
+            {
+                Items = new List<OfertaItemDTO>()
+            };
+        }
+    }
+}
