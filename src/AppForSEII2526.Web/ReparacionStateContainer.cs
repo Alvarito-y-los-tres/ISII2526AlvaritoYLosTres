3585a5e9555e5ab45d5f11107b5493e3377b4cb7
martinucho1885 <martin.alvarez@alu.uclm.es>
2025-12-02 16:19:46 +0100

    todo arreglado (#203)

--- src/AppForSEII2526.Web/ReparacionStateContainer.cs
+++ src/AppForSEII2526.Web/ReparacionStateContainer.cs
@@ -1,53 +1,54 @@
 using AppForSEII2526.Web.API;
+using System.Collections.Generic;
+using System;
+using System.Linq;
 
-namespace AppForSEII2526.Web {
-    public class ReparacionStateContainer {
-
-        //we create an instance of Rental when an instance of RentalStateContainer is created
+namespace AppForSEII2526.Web 
+{
+    public class ReparacionStateContainer 
+    {
+        // Inicializamos la lista para evitar NullReferenceException
         public CrearReparacionDTO Reparacion { get; private set; } = new CrearReparacionDTO() {
             Items = new List<ReparacionItemDTO>()
         };
-
-        // aqui calcula el precio pero no lo voy a poner de momento pq no se para que es
-
 
         public event Action? OnChange;
 
         private void NotifyStateChanged() => OnChange?.Invoke();
 
-
-        
-        public void AddMItemReparacion(ReparacionItemDTO item) {
-            //before adding a movie we checked whether it has been already added
-            if (!Reparacion.Items.Any(ri => ri.NombreHerramienta == item.NombreHerramienta))
-                //we add it if it is not in the list
+        public void AddItemReparacion(HerramientaRepararDTO item) 
+        {
+            // Verificamos si ya existe para no duplicar
+            if (!Reparacion.Items.Any(ri => ri.NombreHerramienta == item.Nombre))
+            {
                 Reparacion.Items.Add(new ReparacionItemDTO() {
-                    NombreHerramienta = item.NombreHerramienta,
-					DescripcionProblema = item.DescripcionProblema,
-					Cantidad = item.Cantidad
-                }
-            );
-
+                    NombreHerramienta = item.Nombre,
+                    DescripcionProblema = "nosesabe",
+                    Cantidad = 1
+                });
+                NotifyStateChanged();
+            }
         }
 
-        //to delete movies from the list of selected movies
-        public void RemoveItemReparacion(ReparacionItemDTO item) {
+        public void RemoveItemReparacion(ReparacionItemDTO item) 
+        {
             Reparacion.Items.Remove(item);
-
+            NotifyStateChanged();
         }
 
-        //we eliminate all the movies from the list
-        public void ClearALL() {
+        public void ClearALL() 
+        {
             Reparacion.Items.Clear();
-
+            NotifyStateChanged();
         }
 
-        //we have already finished the process of renting, thus, we create a new Rental 
-        public void RentalProcessed() {
-            //we have finished the rental process so we create a new object without data
+        // Reseteamos el estado después de procesar
+        public void ReparacionProcessed() 
+        {
             Reparacion = new CrearReparacionDTO() {
                 Items = new List<ReparacionItemDTO>()
             };
+            NotifyStateChanged();
         }
     }
 }
