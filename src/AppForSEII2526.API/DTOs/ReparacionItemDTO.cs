f575ea8624f751d4ebac79de16d7444336774ebf
martinucho1885 <martin.alvarez@alu.uclm.es>
2025-12-07 10:56:33 +0100

    he hecho que se muestre el precio y el id de la reparacion , he arreglado las pruebas (#219)
    Co-authored-by: Guillermo Fernandez <GuillermoTomas.Fernandez@alu.uclm.es>

--- src/AppForSEII2526.API/DTOs/ReparacionItemDTO.cs
+++ src/AppForSEII2526.API/DTOs/ReparacionItemDTO.cs
@@ -1,17 +1,27 @@
+
+using System.ComponentModel;
+
 namespace AppForSEII2526.API.DTOs
 {
+    
     public class ReparacionItemDTO
     {
         // nombre precio descripcionProblema cantidad
         public string NombreHerramienta { get; set; }
         public string DescripcionProblema { get; set; }
         public int Cantidad { get; set; }
+        
+        public decimal Precio { get; set; }
+       
+        public int ReparacionId { get; set; }
 
-        public ReparacionItemDTO(string nombreHerramienta, string descripcionProblema, int cantidad)
+        public ReparacionItemDTO(string nombreHerramienta, string descripcionProblema, int cantidad, decimal precio, int reparacionId)
         {
             NombreHerramienta = nombreHerramienta;
             DescripcionProblema = descripcionProblema;
             Cantidad = cantidad;
+            Precio = precio;
+            ReparacionId = reparacionId;
         }
 
         public override bool Equals(object? obj)
@@ -19,12 +29,14 @@ namespace AppForSEII2526.API.DTOs
             return obj is ReparacionItemDTO dTO &&
                    NombreHerramienta == dTO.NombreHerramienta &&
                    DescripcionProblema == dTO.DescripcionProblema &&
-                   Cantidad == dTO.Cantidad;
+                   Cantidad == dTO.Cantidad &&
+                   Precio == dTO.Precio &&
+                   ReparacionId == dTO.ReparacionId;
         }
 
         public override int GetHashCode()
         {
-            return HashCode.Combine(NombreHerramienta, DescripcionProblema, Cantidad);
+            return HashCode.Combine(NombreHerramienta, DescripcionProblema, Cantidad, Precio,ReparacionId);
         }
 
     }
