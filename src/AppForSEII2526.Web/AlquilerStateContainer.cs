50ede680d99f1051babf171ba9e80ebbd0f3cfa9
Daniel de la Cruz Torrijos <Daniel.Cruz3@alu.uclm.es>
2025-12-08 12:57:22 +0100

    Cambios implementados para laUI del select del CU AlquilarHerramienta (#228)

---
+++ src/AppForSEII2526.Web/AlquilerStateContainer.cs
@@ -0,0 +1,58 @@
+﻿
+using AppForSEII2526.Web.API;
+
+
+namespace AppForSEII2526.Web
+{
+    public class AlquilerStateContainer
+    {
+        public CrearAlquilerDTO Alquiler { get; private set; } = new CrearAlquilerDTO()
+        {
+            AlquilerItems = new List<AlquilerItemDTO>()
+        };
+
+        public decimal PrecioFinal
+        {
+            get
+            {
+                return (decimal)Alquiler.AlquilerItems.Sum(i => i.PrecioHerramienta);
+            }
+        }
+
+        public event Action? OnChange;
+
+        public void NotifyStateChanged() => OnChange?.Invoke();
+
+        public void AñadirHerramientaToAlquiler(HerramientaAlquilarDTO herramienta)
+        {
+            if (!Alquiler.AlquilerItems.Any(i => i.NombreHerramienta == herramienta.Nombre))
+            {
+                Alquiler.AlquilerItems.Add(new AlquilerItemDTO()
+                {
+                    NombreHerramienta = herramienta.Nombre,
+                    MaterialHerrramienta = herramienta.Material,
+                    PrecioHerramienta = herramienta.Precio
+                });
+                NotifyStateChanged();
+            }
+        }
+
+        public void EliminarHerramientaDeAlquiler(AlquilerItemDTO item)
+        {
+            Alquiler.AlquilerItems.Remove(item);
+        }
+
+        public void LimpiarCarritoAlquiler()
+        {
+            Alquiler.AlquilerItems.Clear();
+        }
+
+        public void AlquilerPreparado()
+        {
+            Alquiler = new API.CrearAlquilerDTO()
+            {
+                AlquilerItems = new List<AlquilerItemDTO>()
+            };
+        }
+    }
+}
