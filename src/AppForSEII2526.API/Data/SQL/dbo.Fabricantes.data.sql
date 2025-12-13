8aa295d9201cc20220396c0c4bd509bd3965824f
Álvaro Cano Andrés <alvaro.cano1@alu.uclm.es>
2025-12-13 23:25:54 +0100

    Archivo SQL sin tildes (#258)

--- src/AppForSEII2526.API/Data/SQL/dbo.Fabricantes.data.sql
+++ src/AppForSEII2526.API/Data/SQL/dbo.Fabricantes.data.sql
@@ -1,5 +1,5 @@
 ﻿SET IDENTITY_INSERT [dbo].[Fabricantes] ON
-INSERT INTO [dbo].[Fabricantes] ([Id], [Nombre]) VALUES (1, N'Ferretería López')
-INSERT INTO [dbo].[Fabricantes] ([Id], [Nombre]) VALUES (2, N'Ferretería García')
-INSERT INTO [dbo].[Fabricantes] ([Id], [Nombre]) VALUES (3, N'Ferretería Ruiz')
+INSERT INTO [dbo].[Fabricantes] ([Id], [Nombre]) VALUES (1, N'Ferreteria Lopez')
+INSERT INTO [dbo].[Fabricantes] ([Id], [Nombre]) VALUES (2, N'Ferreteria Garcia')
+INSERT INTO [dbo].[Fabricantes] ([Id], [Nombre]) VALUES (3, N'Ferreteria Ruiz')
 SET IDENTITY_INSERT [dbo].[Fabricantes] OFF
