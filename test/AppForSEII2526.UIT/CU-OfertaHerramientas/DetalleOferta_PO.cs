cf4e5bdf8eb3ddca9f94c0f23defd5cc768ea078
Álvaro Cano Andrés <alvaro.cano1@alu.uclm.es>
2025-12-13 22:44:54 +0100

    240 creación de pruebas funcionales con selenium de las interfaces de la oferta (#250)
    * modificaciones necesarias
    
    * creacion de las pruebas funcionales con selenium

---
+++ test/AppForSEII2526.UIT/CU-OfertaHerramientas/DetalleOferta_PO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+
+namespace AppForSEII2526.UIT.CU_OfertaHerramientas
+{
+    public class DetalleOferta_PO : PageObject
+    {
+        private By detallesOfertaTable = By.Id("HerramientasEnOferta");
+
+        public DetalleOferta_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+
+        public bool CheckOfertaCreadaCorrectamente(List<string[]> expectedDetalles)
+        {
+            return CheckBodyTable(expectedDetalles, detallesOfertaTable);
+        }
+    }
+}
---
+++ test/AppForSEII2526.UIT/CU-OfertaHerramientas/SelectHerramientasParaOfertar_PO.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AppForSEII2526.UIT.CU_OfertaHerramientas
+{
+    internal class SelectHerramientasParaOfertar_PO : PageObject
+    {
+        private By inputFabricante = By.Id("inputFabricante");
+        private By botonBuscar = By.Id("buscarHerraminetas");
+        private By tablaHerramientas = By.Id("Tabla de herramientas");
+        private By botonContinuar = By.Id("ofertarHerramientasBoton");
+
+        public SelectHerramientasParaOfertar_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+
+        public void BuscarHerramientas(string fabricante)
+        {
+            WaitForBeingVisible(inputFabricante);
+
+            var elementoInput = _driver.FindElement(inputFabricante);
+
+            elementoInput.Clear();
+            elementoInput.SendKeys(fabricante);
+            _driver.FindElement(botonBuscar).Click();
+        }
+
+        public bool CheckListaHerramientasCargadas(List<string[]> expectedHerramientas)
+        {
+            return CheckBodyTable(expectedHerramientas, tablaHerramientas);
+        }
+
+        public void EliminarHerramienta(string nombreHerramienta)
+        {
+            By botonEliminarHerramienta = By.Id($"eliminarHerramienta_{nombreHerramienta}");
+
+            WaitForBeingClickable(botonEliminarHerramienta);
+            _driver.FindElement(botonEliminarHerramienta).Click();
+        }
+
+        public bool CheckCarritoVacio()
+        {
+            var botonContinuar = _driver.FindElements(By.Id("ofertarHerramientasBoton"));
+
+            if (botonContinuar.Count > 0 && botonContinuar[0].Displayed)
+            {
+                return false;
+
+                throw new Exception("Error: El carrito debería estar vacío, pero el botón 'Continuar' es visible.");
+            }
+            return true;
+        }
+
+        public void AñadirHerramientaAlCarrito(string nombreHerramienta)
+        {
+            By btnAddLocator = By.Id($"herramientaParaOferta_{nombreHerramienta}");
+
+            WaitForBeingClickable(btnAddLocator);
+            _driver.FindElement(btnAddLocator).Click();
+
+            By btnRemoveLocator = By.Id($"eliminarHerramienta_{nombreHerramienta}");
+
+            WaitForBeingClickable(btnRemoveLocator);
+        }
+
+        public void ContinuarConOferta()
+        {
+            WaitForBeingClickable(botonContinuar);
+            _driver.FindElement(botonContinuar).Click();
+        }
+    }
+}
